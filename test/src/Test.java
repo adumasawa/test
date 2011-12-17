d2e40019ff801e72f8e8818804cb8703298c2e85
adumasawa <yanato_2@yahoo.co.jp>
2011-12-17 23:01:49 +0900

    test0

--- test/src/Test.java
+++ test/src/Test.java
@@ -3,26 +3,29 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("hello world.");
 	}
-	
+
 	public void method1(){
-		
+
 	}
 	public void method2(){
-		
+
 	}
 	public void method3(){
-		
+
 	}
 	public void method4(){
-		
+
 	}
 	public void method5(){
-		
+
 	}
 	public void method6(){
-		
+
 	}
 	public void method7(){
-		
+
+	}
+	public void method9(){
+
 	}
 }
